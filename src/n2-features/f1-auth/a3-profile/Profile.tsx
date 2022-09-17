ef86a50be54d48ca8bcf3228eb15c64706a59198
Андрей Ашмянец <andreka6033@gmail.com>
2022-09-17 15:00:26 +0300

    new login title

--- src/n2-features/f1-auth/a3-profile/Profile.tsx
+++ src/n2-features/f1-auth/a3-profile/Profile.tsx
@@ -1,8 +1,15 @@
 import React from "react";
+import Box from "@mui/material/Box";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Profile = () => {
     return (
-        <div>Profile</div>
+        <div>Login new</div>
     )
 }
 
